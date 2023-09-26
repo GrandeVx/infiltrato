1c4f2d662b2b7400eeec2030fe2dccfb15615a87
Vittorio D'Alfonso <vittoriodalfonso07@gmail.com>
2023-09-26 09:46:51 +0200

    Updated User TRPC

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,43 +1,74 @@
 import { z } from "zod";
-
-import { createTRPCRouter, publicProcedure , privateProcedure} from "@/server/api/trpc";
+import { clerkClient } from "@clerk/nextjs";
+import { TRPCError } from "@trpc/server";
+import {
+  createTRPCRouter,
+  publicProcedure,
+  privateProcedure,
+} from "@/server/api/trpc";
 
 export const userProcedure = createTRPCRouter({
 
-    getAllUsers : publicProcedure
-        .query(async ({ ctx }) => {
-        return await ctx.prisma.user.findMany(
-            {
-                select : {
-                    id : true,
-                    domain : true,
-                    province : true,
-                    city : true,
-                },
+    getAllUsers: publicProcedure.query(async ({ ctx }) => {
+        return await ctx.prisma.user.findMany({
+        select: {
+            id: true,
+            domain: true,
+            province: true,
+            city: true,
+        },
         });
     }),
 
-    getUser : privateProcedure
-        .input(z.object({
-            id : z.string(),
-        }))
-        .query(async ({ ctx }) => {
-        return await ctx.prisma.user.findMany(
-            {
-                select : {
-                    id : true,
-                    domain : true,
-                    province : true,
-                    city : true,
-                    subs : true,
-                    messages : true,
-                    comments : true,
-                },
+    getUser: privateProcedure.query(async ({ ctx }) => {
+        const user = await ctx.prisma.user.findMany({
+        select: {
+            id: true,
+            domain: true,
+            province: true,
+            city: true,
+            subs: true,
+            messages: true,
+            comments: true,
+        },
 
-                where : {
-                    id : ctx.currentUser,
-                }
+        where: {
+            id: ctx.currentUser,
+        },
         });
+        return user[0];
     }),
 
-});
+    createUser: privateProcedure.input(
+        z.object({
+            domain: z.string(),
+            province: z.string(),
+            city: z.string(),
+            name: z.string(),
+            surname: z.string(),
+        }),
+    ).mutation(async ({ ctx, input }) => {
+
+        const user = await ctx.prisma.user.create({
+          data: {
+            name: input.name,
+            surname: input.surname,
+            authId: ctx.currentUser,
+            domain: input.domain,
+            province: input.province,
+            city: input.city,
+          },
+        }).catch((err) => {
+            throw new TRPCError({
+                code: "BAD_REQUEST",
+                message: err.message,
+            });
+            }
+        );
+
+      return user;
+    }),
+
+    
+
+    });
